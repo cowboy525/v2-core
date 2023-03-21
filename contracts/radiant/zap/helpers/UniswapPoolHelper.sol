e71584f4583969e76e868633be6fccdc5b7daafb
RadiantCapitalDevelopment <104457735+RadiantCapitalDevelopment@users.noreply.github.com>
2023-03-20 23:14:19 -0400

    Update UniswapPoolHelper.sol

--- contracts/radiant/zap/helpers/UniswapPoolHelper.sol
+++ contracts/radiant/zap/helpers/UniswapPoolHelper.sol
@@ -1,12 +1,10 @@
-// SPDX-License-Identifier: agpl-3.0
-pragma solidity 0.8.4;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.12;
 pragma abicoder v2;
 
 import "./DustRefunder.sol";
-
 import "@uniswap/lib/contracts/libraries/UniswapV2Library.sol";
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
-
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
@@ -32,6 +30,7 @@ contract UniswapPoolHelper is Initializable, OwnableUpgradeable, DustRefunder {
 
 	IUniswapV2Router02 public router;
 	ILiquidityZap public liquidityZap;
+	address public lockZap;
 
 	function initialize(
 		address _rdntAddr,
@@ -73,12 +72,13 @@ contract UniswapPoolHelper is Initializable, OwnableUpgradeable, DustRefunder {
 		IERC20 lp = IERC20(lpTokenAddr);
 		lp.safeTransfer(msg.sender, lp.balanceOf(address(this)));
 
-		liquidityZap.initLiquidityZap(rdntAddr, wethAddr, lpTokenAddr);
+		liquidityZap.initLiquidityZap(rdntAddr, wethAddr, lpTokenAddr, address(this));
 	}
 
 	function zapWETH(uint256 amount) public returns (uint256 liquidity) {
-		IWETH WETH = IWETH(wethAddr);
-		WETH.transferFrom(msg.sender, address(liquidityZap), amount);
+		require(msg.sender == lockZap, "!lockZap only");
+		IWETH weth = IWETH(wethAddr);
+		weth.transferFrom(msg.sender, address(liquidityZap), amount);
 		liquidity = liquidityZap.addLiquidityWETHOnly(amount, payable(address(this)));
 		IERC20 lp = IERC20(lpTokenAddr);
 
@@ -87,15 +87,7 @@ contract UniswapPoolHelper is Initializable, OwnableUpgradeable, DustRefunder {
 		refundDust(rdntAddr, wethAddr, msg.sender);
 	}
 
-	function getReserves()
-		public
-		view
-		returns (
-			uint256 rdnt,
-			uint256 weth,
-			uint256 lpTokenSupply
-		)
-	{
+	function getReserves() public view returns (uint256 rdnt, uint256 weth, uint256 lpTokenSupply) {
 		IUniswapV2Pair lpToken = IUniswapV2Pair(lpTokenAddr);
 
 		(uint256 reserve0, uint256 reserve1, ) = lpToken.getReserves();
@@ -122,21 +114,22 @@ contract UniswapPoolHelper is Initializable, OwnableUpgradeable, DustRefunder {
 		uint sqrtK = HomoraMath.sqrt(rdntReserve.mul(wethReserve)).fdiv(lpSupply); // in 2**112
 
 		// rdnt in eth, decis 8
-		uint px0 = rdntPriceInEth.mul(2**112); // in 2**112
+		uint px0 = rdntPriceInEth.mul(2 ** 112); // in 2**112
 		// eth in eth, decis 8
-		uint px1 = uint256(100000000).mul(2**112); // in 2**112
+		uint px1 = uint256(100000000).mul(2 ** 112); // in 2**112
 
 		// fair token0 amt: sqrtK * sqrt(px1/px0)
 		// fair token1 amt: sqrtK * sqrt(px0/px1)
 		// fair lp price = 2 * sqrt(px0 * px1)
 		// split into 2 sqrts multiplication to prevent uint overflow (note the 2**112)
-		uint result = sqrtK.mul(2).mul(HomoraMath.sqrt(px0)).div(2**56).mul(HomoraMath.sqrt(px1)).div(2**56);
-		priceInEth = result.div(2**112);
+		uint result = sqrtK.mul(2).mul(HomoraMath.sqrt(px0)).div(2 ** 56).mul(HomoraMath.sqrt(px1)).div(2 ** 56);
+		priceInEth = result.div(2 ** 112);
 	}
 
 	function zapTokens(uint256 _wethAmt, uint256 _rdntAmt) public returns (uint256 liquidity) {
-		IWETH WETH = IWETH(wethAddr);
-		WETH.transferFrom(msg.sender, address(this), _wethAmt);
+		require(msg.sender == lockZap, "!lockZap only");
+		IWETH weth = IWETH(wethAddr);
+		weth.transferFrom(msg.sender, address(this), _wethAmt);
 		IERC20(rdntAddr).safeTransferFrom(msg.sender, address(this), _rdntAmt);
 		liquidity = liquidityZap.standardAdd(_rdntAmt, _wethAmt, address(this));
 		IERC20 lp = IERC20(lpTokenAddr);
@@ -155,5 +148,14 @@ contract UniswapPoolHelper is Initializable, OwnableUpgradeable, DustRefunder {
 
 	function setLiquidityZap(address _liquidityZap) external onlyOwner {
 		liquidityZap = ILiquidityZap(_liquidityZap);
+	}
+
+	function setLockZap(address _lockZap) external onlyOwner {
+		lockZap = _lockZap;
+	}
+
+	function getPrice() public view returns (uint256 priceInEth) {
+		// TODO: look at reserves
+		priceInEth = 7500;
 	}
 }
