71c69b47d59abd85b948793a12be587232e01e6f
leggio <harrison@legg.io>
2023-04-16 09:30:05 -0400

    fix: unchecked iterator in hasEligibleDeposits

--- contracts/radiant/staking/ChefIncentivesController.sol
+++ contracts/radiant/staking/ChefIncentivesController.sol
@@ -519,11 +519,14 @@ contract ChefIncentivesController is Initializable, PausableUpgradeable, Ownable
 
 	function hasEligibleDeposits(address _user) internal view returns (bool hasDeposits) {
 		uint256 length = poolLength();
-		for (uint256 i; i < length; i++) {
+		for (uint256 i; i < length;) {
 			if (userInfo[registeredTokens[i]][_user].amount != 0) {
 				hasDeposits = true;
 				break;
 			}
+      unchecked {
+        ++i;
+      }
 		}
 	}
 
