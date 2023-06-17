503383f32dbbb2e0a553efd6c64547176ff0f76a
freshpizza <fpizza@protonmail.com>
2023-06-17 16:17:33 -0600

    prevent dust production in zapFromVesting

--- contracts/radiant/zap/LockZap.sol
+++ contracts/radiant/zap/LockZap.sol
@@ -208,7 +208,7 @@ contract LockZap is Initializable, OwnableUpgradeable, PausableUpgradeable, Dust
 	 */
 	function zapFromVesting(bool _borrow, uint256 _lockTypeIndex) public payable whenNotPaused returns (uint256) {
 		uint256 rdntAmt = mfd.zapVestingToLp(msg.sender);
-		uint256 wethAmt = quoteFromToken(rdntAmt);
+		uint256 wethAmt = poolHelper.quoteFromToken(rdntAmt);
 		return _zap(_borrow, wethAmt, rdntAmt, address(this), msg.sender, _lockTypeIndex, msg.sender);
 	}
 
