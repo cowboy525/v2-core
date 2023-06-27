d74b5e8df007e07eaae4e89a2f663836400b8903
SunlightLuck <sunlight.luck@gmail.com>
2023-06-27 12:47:08 -0400

    add test scenario for swap failed

--- test/bounties/ac-bounty.test.ts
+++ test/bounties/ac-bounty.test.ts
@@ -63,13 +63,13 @@ const generatePlatformRevenue = async (duration: number = SKIP_DURATION) => {
 };
 
 const generateGiganticPlatformRevenue = async (duration: number = SKIP_DURATION) => {
-	await deposit('rUSDT', '200000000000000000', deployer, lendingPool, deployData);
+	await deposit('rWBTC', '90000000000000000000000000000000000', deployer, lendingPool, deployData);
 
-	await doBorrow('rUSDT', '100000000000000000', deployer, lendingPool, deployData);
+	await doBorrow('rWBTC', '20000000000000000000000000000000000', deployer, lendingPool, deployData);
 
 	await advanceTimeAndBlock(duration);
 
-	await doBorrow('rUSDT', '10000000000000000', deployer, lendingPool, deployData);
+	await doBorrow('rWBTC', '100000', deployer, lendingPool, deployData);
 
 
 	await multiFeeDistribution.connect(deployer).getAllRewards();
@@ -187,7 +187,6 @@ describe(`AutoCompound:`, async () => {
 		
 		const expectedFee = await getPendingInRdnt();
 		const quote = await bountyManager.connect(hunter).quote(user1.address);
-		await generateGiganticPlatformRevenue();
 
 		await bountyManager.connect(hunter).claim(user1.address, quote.actionType);
 
@@ -227,5 +226,12 @@ describe(`AutoCompound:`, async () => {
 		await generatePlatformRevenue(1 * HOUR);
 		let fee = await compounder.connect(user1).selfCompound();
 		await expect(fee.value).to.be.equal(0);
+	});
+
+	it('swap failed', async () => {
+		const quote = await bountyManager.connect(hunter).quote(user1.address);
+		await generateGiganticPlatformRevenue();
+
+		await expect(bountyManager.connect(hunter).claim(user1.address, quote.actionType)).to.be.revertedWith('SwapFailed');
 	});
 });
