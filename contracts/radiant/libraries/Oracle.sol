106ce439e7e27066846b5be7456238e04197002b
pwollemi <ylogan.sa@gmail.com>
2023-07-07 11:41:44 -0400

    Chainlink round checking - move to library

---
+++ contracts/radiant/libraries/Oracle.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.12;
+
+import {AggregatorV3Interface} from "../../interfaces/AggregatorV3Interface.sol";
+
+library Oracle {
+	/// @notice The period for price update, this is taken from heartbeats of chainlink price feeds
+	uint256 public constant UPDATE_PERIOD = 86400;
+
+	error RoundNotComplete();
+
+	error StalePrice();
+
+	error InvalidPrice();
+
+	function getAnswer(AggregatorV3Interface chainlinkFeed) internal view returns (int256) {
+		(, int256 answer, , uint256 updatedAt, ) = chainlinkFeed.latestRoundData();
+		if(updatedAt == 0) revert RoundNotComplete();
+		if(block.timestamp - updatedAt >= UPDATE_PERIOD) revert StalePrice();
+		if(answer <= 0) revert InvalidPrice();
+		return answer;
+	}
+}
